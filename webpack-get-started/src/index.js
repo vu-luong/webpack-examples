349af4ee5b2515cc88407aabb27b8a9620ca8b51
vu-luong <vubinhcht@gmail.com>
2022-01-11 22:44:40 +1000

    Asset management

--- webpack-get-started/src/index.js
+++ webpack-get-started/src/index.js
@@ -1,9 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpeg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
-function component() {
+
+export function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // Add the image to our existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
--- webpack-get-started/webpack.config.js
+++ webpack-get-started/webpack.config.js
@@ -4,7 +4,27 @@ module.exports = {
     entry: './src/index.js',
     mode: 'development',
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ]
+    }
 };
